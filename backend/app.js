fe345b78490957f96f38a0801377055dceb8ab25
Amruth Shyju <amruthshyju@gamil.com>
2024-06-28 16:51:41 +0530

    Mongodb Connected

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 dotenv.config()
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+
+
+
+
+
+mongoose.connect(`mongodb+srv://amruthshyju:${process.env.MONGODB_PASSWORD}@cluster0.i8yx2tw.mongodb.net/?retryWrites=true&w=majority&appName=WatchHaven`)
+  .then(()=>{
+    app.listen(PORT,(err)=>{
+            if(!err){
+                console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+            }else{
+                console.log("Error occurred, server can't start", error);
+            }
+    })
+  })
+
+
+ 
